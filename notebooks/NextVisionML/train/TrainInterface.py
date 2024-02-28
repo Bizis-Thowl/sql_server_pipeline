315439595e3ed12a03e13ad8cb60ba23ab6e555f
TheOnlyZipperlein <40144305+TheOnlyZipperlein@users.noreply.github.com>
2024-02-28 16:02:22 +0100

    update

--- notebooks/NextVisionML/train/TrainInterface.py
+++ notebooks/NextVisionML/train/TrainInterface.py
@@ -1,12 +1,15 @@
+import pandas as pd
+
 class TrainInterface:
     def __init__(self, mlContext):
         self.mlContext = mlContext
-        
-    def claculate(self, i, args): # Derive Hyperopt args from method not from context.iter_args[i]!!!!!
-        pass
     
     def populate(self, i):
         pass
     
     def upload(self, i):
-        pass
+        df = pd.DataFrame()
+        df["pred"] = self.mlContext.iter_objs[i]["dtc_pred"]
+        df["datapoint_id"] = self.mlContext.test_db_indexes
+        df["model_id"] = self.mlContext.iter_objs[i]["model"]["dtc"].id
+        df.to_sql("prediciions_categorical", con = self.mlContext.engine, index = False, if_exists='append')
--- notebooks/NextVisionML/train/interfaces/train/DecisionTreeClfr copy.py
+++ notebooks/NextVisionML/train/interfaces/train/DecisionTreeClfr copy.py
@@ -18,14 +18,21 @@ class DecisionTreeClfr(TrainInterface):
             max_features = args["max_features"],
             criterion = args["criterion"],
         )
+        self.mlContext.iter_objs[i]["hyperparameter"] = update_object_attributes(self.mlContext.context, self.mlContext.iter_objs[i]["hyperparameter"], 
+            max_depth = int(args["max_depth"]),
+            min_samples_leaf = int(args["min_samples_leaf"]),
+            random_state = int(args["random_state"]),
+            max_features = int(args["max_features"]),
+            criterion = args["criterion"],    
+        )
         return dtc
-        dtc.fit(self.mlContext.iter_train_X[i], self.mlContext.iter_train_y[i])
-        self.mlContext.iter_objs[i]["model"]["dtc"] = dtc
-        eval_predict = dtc.predict(self.mlContext.iter_test_X[i]) 
-        balanced_accuracy = balanced_accuracy_score(self.mlContext.iter_test_y[i], eval_predict) 
+        #dtc.fit(self.mlContext.iter_train_X[i], self.mlContext.iter_train_y[i])
+        #self.mlContext.iter_objs[i]["model"]["dtc"] = dtc
+        #eval_predict = dtc.predict(self.mlContext.iter_test_X[i]) 
+        #balanced_accuracy = balanced_accuracy_score(self.mlContext.iter_test_y[i], eval_predict) 
         
-        self.mlContext.iter_objs[i]["dtc_pred"] = eval_predict
-        self.mlContext.iter_objs[i]["dtc_balanced_accuracy"] = balanced_accuracy
+        #self.mlContext.iter_objs[i]["dtc_pred"] = eval_predict
+        #self.mlContext.iter_objs[i]["dtc_balanced_accuracy"] = balanced_accuracy
         
         test_pred = pd.DataFrame(eval_predict.reshape(-1, 1))  
         class_mapping = {"low": 0, "low-med": 1, "medium": 2, "med-high": 3, "high": 4}
@@ -33,15 +40,7 @@ class DecisionTreeClfr(TrainInterface):
         test_pred = test_pred[0].map(class_mapping)
         self.mlContext.iter_objs[i]["dtc_trust_scores"] = get_trust_scores(self.mlContext.iter_train_X[i].values, train_y_, self.mlContext.iter_test_X[i].values, test_pred)
         
-        self.mlContext.iter_objs[i]["hyperparameter"] = update_object_attributes(self.mlContext.context, self.mlContext.iter_objs[i]["hyperparameter"], 
-            max_depth = int(args["max_depth"]),
-            min_samples_leaf = int(args["min_samples_leaf"]),
-            random_state = int(args["random_state"]),
-            max_features = int(args["max_features"]),
-            criterion = args["criterion"],    
-        ) 
-        
-        return balanced_accuracy        
+    
         
     def populate(self, i):
         args = {
@@ -55,6 +54,7 @@ class DecisionTreeClfr(TrainInterface):
         self.mlContext.iter_objs[i]["model"]["dtc"] = update_object_attributes(self.mlContext.context, self.mlContext.iter_objs[i]["model"]["dtc"],
                                                                         path_to_model = "dtc")
     def upload(self, i):
+        super().upload()
         self.mlContext.iter_objs[i]["model_score_dtc"] = update_object_attributes(self.mlContext.context, self.mlContext.iter_objs[i]["model_score_dtc"],
             balanced_accuracy_score = self.mlContext.iter_objs[i]["dtc_balanced_accuracy"])
         
